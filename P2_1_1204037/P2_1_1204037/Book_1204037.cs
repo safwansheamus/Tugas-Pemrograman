284f530069bc01bf5d6b301dd014d8f5e661816d
safwansheamus <94037698+safwansheamus@users.noreply.github.com>
2021-11-19 23:57:06 +0800

    Add files via upload

---
+++ P2_1_1204037/P2_1_1204037/Book_1204037.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P2_1_1204037
+{
+    class Book_1204037 : Product_1204037
+    {
+        protected string PageCount;
+        public Book_1204037(string type, string title, string PageCount) : base (type, title)
+        {
+            this.pageCount = PageCount;
+        }
+
+        public string pageCount
+        {
+            get
+            {
+                return PageCount;
+            }
+
+            set
+            {
+                PageCount = value;
+            }
+        }
+    }
+}
---
+++ P2_1_1204037/P2_1_1204037/DVD_1204037.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P2_1_1204037
+{
+    class DVD_1204037: Product_1204037
+    {
+        protected string duration;
+
+        public DVD_1204037(string title, string duration)
+        {
+            this.MyTitle = title;
+            this.duration = duration;
+            this.MyType = "DVD";
+
+            Console.WriteLine(" Ini dari class DVD");
+        }
+        public string Duration
+        {
+            get
+            {
+                return duration;
+            }
+            set
+            {
+                duration = value;
+            }
+        }
+
+     }
+    }
+
