70378cb645a7a91ee7cc11230893197b628df868
safwansheamus <94037698+safwansheamus@users.noreply.github.com>
2021-11-20 09:30:21 +0800

    Add files via upload

---
+++ P2_2_1204037/P2_2_1204037/Hewan.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P2_2_1204037
+{
+    class Hewan
+    {
+        public string namaHewan;
+        public int tinggiHewan;
+        public string jenisHewan;
+
+        public void tampilkannamahewan()
+        {
+            Console.WriteLine("nama hewan:  " + this.namaHewan);
+        }
+
+        public void tampilkanjenishewan()
+        {
+            Console.WriteLine("jenis hewan: " + this.jenisHewan);
+        }
+
+        public int tampilkantinggihewan()
+        {
+            int tinggiHewan = this.tinggiHewan;
+            return tinggiHewan;
+        }
+    }
+}
