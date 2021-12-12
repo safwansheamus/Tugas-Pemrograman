9eca346bc9ce3a0c4f70175014a653eeec05c287
safwansheamus <94037698+safwansheamus@users.noreply.github.com>
2021-12-12 12:08:16 +0800

    Add files via upload

---
+++ Program Cs P_2/P2_1_1204037/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P2_1_1204037
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ Program Cs P_2/P2_2_1204037/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P2_2_1204037
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hewan jerapah = new Hewan();
+
+            jerapah.namaHewan = "jerapah afrika";
+            jerapah.jenisHewan = "jantan";
+            jerapah.tinggiHewan = 10;
+
+            jerapah.tampilkannamahewan();
+            Console.WriteLine("Jenis kelamin: " + jerapah.jenisHewan);
+            jerapah.tampilkantinggihewan();
+            Console.Read();
+        }
+    }
+}
